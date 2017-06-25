20ace2e009210be6b877d1c476b3ba64b5c67ab8
bdevapatla <reachbp4@gmail.com>
2017-06-25 14:11:20 -0400

    Week-2 assignment

---
+++ Algorithms-1/week-2/queues/Deque.java
@@ -0,0 +1,124 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    
+    private Node first, last; 
+    private int size;
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node previous;
+    }
+    
+    public Deque() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new java.lang.IllegalArgumentException("Insertion of NULL element is not supported.");        
+        }
+        
+        Node oldFirst =  first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;      
+        if (size == 0) {
+            last = first;
+        }
+        else {
+            oldFirst.previous = first;
+        }
+        size++;
+    }
+    
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new java.lang.IllegalArgumentException("Insertion of NULL element is not supported.");        
+        }
+        else if (size == 0) {
+            addFirst(item);
+        }
+        else {
+            Node oldLast =  last;       
+            last = new Node();
+            last.item = item;       
+            last.previous = oldLast;        
+            oldLast.next = last;        
+            size++;
+        }
+    }
+    
+    public Item removeFirst() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException("Empty dequeue");
+        }
+        size--;
+        Item item = first.item;
+        first = first.next;
+        if (first != null) {
+            first.previous = null;
+        }
+        return item;
+    }
+    
+    public Item removeLast() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException("Empty dequeue");
+        }
+        else if (size == 1) {
+            return removeFirst();
+        }
+        else {
+            size--;
+            Item item = last.item;
+            last = last.previous;
+            last.next = null;
+            return item;
+        }
+    }
+    
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }   
+    
+    private class DequeIterator implements Iterator<Item> {
+        private Node current;
+        
+        public DequeIterator() {
+            current = first;
+        }
+        
+        public boolean hasNext() {      
+            return current != null;
+        }
+
+        public Item next() {            
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException("Iterator has no more elements");
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException("Remove method is not supported");
+        }
+        
+    }
+    
+
+    public static void main(String[] args) {                
+    }
+}
---
+++ Algorithms-1/week-2/queues/RandomizedQueue.java
@@ -0,0 +1,125 @@
+import java.util.Iterator;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    private Item[] q; 
+    private int n;  
+    private int last;
+    
+    public RandomizedQueue() {
+        q = (Item[]) new Object[1];
+        n = 0;      
+        last = 0;
+    }
+    
+    private void resize(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        int i = 0;
+        for (int j = 0; j < q.length; j++) {
+            if (q[j] != null) {
+                temp[i++] = q[j];
+            }
+        }
+        q = temp;
+        last = n;
+    }
+    
+    public boolean isEmpty() {
+        return n == 0;
+    }
+    
+    public int size() {
+        return n;
+    }
+    
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new java.lang.NullPointerException("Cannot add NULL item to queue");
+        }
+        if (last == q.length) {
+            resize(n*2);
+        }      
+        
+        q[last++] = item;
+        n++;
+    }   
+    
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("No more elements");
+        }
+        Item item = null;       
+        do {
+            int randomIndex = StdRandom.uniform(q.length);
+            if (q[randomIndex] != null) { 
+                item = q[randomIndex];
+                q[randomIndex] = null;
+            }       
+        } while (item == null);          
+        n--;
+        if (n == 0) {
+            last = 0;
+        }
+        if (n > 0 && n == q.length/4) {
+            resize(q.length/2);
+        }
+        return item;
+    }
+    
+    public Item sample() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException("No more elements");
+        }
+        Item item = null;       
+        do {
+            int randomIndex = StdRandom.uniform(q.length);
+            if (q[randomIndex] != null) {                
+                item = q[randomIndex];
+            }           
+        } while (item == null);
+        return item;
+    } 
+     
+    
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+    
+    private class RandomizedQueueIterator implements Iterator<Item> {    
+        
+        private int i;
+        private Item[] nodes;
+        public RandomizedQueueIterator() {
+            i = 0;
+            nodes = (Item[]) new Object[n];
+            int j = 0;
+            for (int k = 0; k < q.length; k++) {
+                if (q[k] != null) {                    
+                    nodes[j++] = q[k];                  
+                }
+            }
+            StdRandom.shuffle(nodes);
+        }
+        
+        public boolean hasNext() {          
+            return i < n;
+        }
+        
+        public Item next() {        
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }            
+            Item item = nodes[i];
+            i++;
+            return item;
+        }
+        
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException(); 
+        }        
+    }
+    
+    public static void main(String[] args) {                
+    }
+}
